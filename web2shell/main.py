7bf9f6bc18e325896e8993fe665aa4c6a2eaced4
Fred <flygg@msn.com>
2023-05-17 11:11:39 +0800

    路由加固

--- web2shell/main.py
+++ web2shell/main.py
@@ -6,7 +6,6 @@ import subprocess
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME'] = 'admin'  # 设置用户名
 app.config['BASIC_AUTH_PASSWORD'] = 'admin'  # 设置密码
-
 basic_auth = BasicAuth(app)
 
 @app.route('/')
@@ -16,14 +15,12 @@ def index():
 
 @app.route('/run_abc_script')
 @basic_auth.required  # 鉴权保护该路由
-
 def run_abc_script():
     subprocess.Popen(['./killst.sh'])    
     return 'abc 脚本已执行'
 
 @app.route('/run_efg_script')
 @basic_auth.required  # 鉴权保护该路由
-
 def run_efg_script():
     subprocess.Popen(['./startst.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return 'efg 脚本已在后台执行'
