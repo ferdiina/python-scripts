8db2d5b4979653eb890cb91afd0c5b0bfbbbcfc9
Fred <flygg@msn.com>
2023-05-17 13:50:35 +0800

    logreader 修改从logfile.txt读取日志列表

--- logreader/main.py
+++ logreader/main.py
@@ -1,36 +1,33 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
+LOG_FILE_PATH = 'logfile.txt'
 
-SOURCE_FILES = [
-    {
-        'name': 'Tomcat-8080',
-        'path': '/opt/tomcat/8080/logs/catalina.out',
-    },
-    {
-        'name': 'Elasticsearch',
-        'path': '/data/elasticsearch/logs/elasticsearch.log',
-    },
-    # Add more source files here
-]
-
-PAGE_SIZE = 1000
 def read_file_content(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
     return content
 
+def read_log_files():
+    files = []
+    with open(LOG_FILE_PATH, 'r') as file:
+        for line in file:
+            name, path = line.strip().split(',')
+            files.append({
+                'name': name,
+                'path': path
+            })
+    return files
 
 @app.route('/', methods=['GET', 'POST'])
 def display_log():
     selected_file = request.args.get('file')
     if selected_file is None:
-        selected_file = SOURCE_FILES[0]['path']
+        selected_file = read_log_files()[0]['path']
 
     log_content = read_file_content(selected_file)
+    source_files = read_log_files()
 
-    return render_template('log.html', log_content=log_content, source_files=SOURCE_FILES, selected_file=selected_file)
-
+    return render_template('log.html', log_content=log_content, source_files=source_files, selected_file=selected_file)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5050)
