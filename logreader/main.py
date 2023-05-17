db6ea88e8f5b7c4b4637924b43d7410521e208a2
Fred <flygg@msn.com>
2023-05-17 13:26:03 +0800

    add logreader2

--- logreader/main.py
+++ logreader/main.py
@@ -15,29 +15,22 @@ SOURCE_FILES = [
 ]
 
 PAGE_SIZE = 1000
-
-
-def read_file_lines(file_path, start_line, end_line):
-    lines = []
+def read_file_content(file_path):
     with open(file_path, 'r') as file:
-        for i, line in enumerate(file):
-            if i >= start_line and i < end_line:
-                lines.append(line.rstrip())
-            elif i >= end_line:
-                break
-    return lines
+        content = file.read()
+    return content
 
 
 @app.route('/', methods=['GET', 'POST'])
 def display_log():
-    selected_file = request.form.get('file')
+    selected_file = request.args.get('file')
     if selected_file is None:
         selected_file = SOURCE_FILES[0]['path']
 
-    lines = read_file_lines(selected_file, 0, PAGE_SIZE)
-    total_lines = sum(1 for _ in open(selected_file))
+    log_content = read_file_content(selected_file)
 
-    return render_template('log.html', lines=lines, total_lines=total_lines, source_files=SOURCE_FILES, selected_file=selected_file)
+    return render_template('log.html', log_content=log_content, source_files=SOURCE_FILES, selected_file=selected_file)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5050)
